9c8e1ac45668a07adfbdec303cb85a87be117a6d
Enrico Comassetto Di Gioia <ricodigioia@gmail.com>
2025-08-26 12:10:25 -0300

    Remove the gap between the tests

--- docs/DecisionTree/arvore-treino4.py
+++ docs/DecisionTree/arvore-treino4.py
@@ -46,9 +46,9 @@ classifier = tree.DecisionTreeClassifier()
 classifier.fit(x_train, y_train)
 
 # Avaliar o modelo
-accuracy = classifier.score(x_test, y_test)
-print(f"Accuracy: {accuracy:.2f}")
-tree.plot_tree(classifier)
+#accuracy = classifier.score(x_test, y_test)
+#print(f"Accuracy: {accuracy:.2f}")
+#tree.plot_tree(classifier)
 
 # Para imprimir na página HTML
 #buffer = StringIO()

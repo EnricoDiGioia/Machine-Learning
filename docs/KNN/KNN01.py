c0c100439349d38d1791a00bd92a6000704c8572
Enrico Comassetto Di Gioia <ricodigioia@gmail.com>
2025-09-09 10:24:56 -0300

    knn

--- docs/KNN/KNN01.py
+++ docs/KNN/KNN01.py
@@ -44,25 +44,3 @@ df_plot['tax'] = (X['tax']-X['tax'].min())/(X['tax'].max()-X['tax'].min())
 df_plot['fuelType'] = y
 sns.scatterplot(data=df_plot, x='price', y='tax', hue='fuelType')
 
-
-#Ver se é necessario dps
-
-# Visualize decision boundary
-#h = 0.02  # Step size in mesh
-#x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-#y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-#xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
-#
-#Z = knn.predict(np.c_[xx.ravel(), yy.ravel()])
-#Z = Z.reshape(xx.shape)
-#
-#plt.contourf(xx, yy, Z, cmap=plt.cm.RdYlBu, alpha=0.3)
-#sns.scatterplot(x=X[:, 0], y=X[:, 1], hue=y, style=y, palette="deep", s=100)
-#plt.xlabel("Feature 1")
-#plt.ylabel("Feature 2")
-#plt.title("KNN Decision Boundary (k=3)")
-#
-## Display the plot
-#buffer = StringIO()
-#plt.savefig(buffer, format="svg", transparent=True)
-#print(buffer.getvalue())
